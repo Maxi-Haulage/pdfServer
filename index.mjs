22476b700d3c5c93dfef2c300010647da0cd6937
Robert Clarkson <robertc60@live.com>
2025-01-03 17:03:44 +0000

    first commit

---
+++ index.mjs
@@ -0,0 +1,41 @@
+import e from "express";
+import cors from "cors";
+
+const app = e()
+app.use(cors())
+app.use(e.json())
+
+var corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
+});
+
+var rawBodyHandler = function (req, res, buf, encoding) {
+  if (buf && buf.length) {
+      req.rawBody = buf.toString(encoding || 'utf8');
+      console.log('Raw body: ' + req.rawBody);
+  }
+}
+
+app.get("/test", (request, response) => {
+  const status = {
+    "Status" : "Running"
+  };
+  response.send(status)
+});
+
+app.post("/SMSIR", cors(corsOptions), function (request, response) {
+  const test = request.body
+  console.log(test.questions[0].file)
+  const status = {
+    "MSG": "data submitted"
+  }
+  response.send(status)
+})
+
